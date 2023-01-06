d450f963cd736bdffdecd9f47a21a3e0e6701577
Kinjal-16 <bhattacharyyakinjal@gmail.com>
2023-01-06 12:26:04 +0000

    New

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -19,9 +19,74 @@ const unsigned int width = 2880;
 const unsigned int height = 1800;
 
 glm::vec3 translate[6], rotation[6];
-int animate(int i,int flag)
+bool collision[6] = {false};
+bool wallCollide[6] = { false };
+float collisionRotaion[] = { 120.0f,120.0f,120.0f,120.0f,120.0f,120.0f };
+float collisiontranslation[] = { 5.0f,5.0f,5.0f,5.0f,5.0f,5.0f };
+
+float collisionWalltranslation[] = { 5.0f,5.0f,5.0f,5.0f,5.0f,5.0f };
+
+
+
+void collisionResolutionSnowman(int i)
 {
+	if (collisionRotaion[i] > 0)
+	{
+		rotation[i].y = rotation[i].y + 0.5;
+		collisionRotaion[i] = collisionRotaion[i] - 0.5;
+	}
+	else if (collisiontranslation[i] > 0)
+	{
+		translate[i].x = translate[i].x + 0.01 * glm::sin(glm::radians(rotation[i].y));
+		translate[i].y = translate[i].y + 0.01 * glm::cos(glm::radians(rotation[i].y));
+		collisiontranslation[i] = collisiontranslation[i] - 0.01;
+	}
+	else
+	{
+		collision[i] = false;
+		collisionRotaion[i] = 120.0f;
+		collisiontranslation[i] = 5.0f;
+	}
+}
+void collisionDetectionSnowman(int k)
+{
+	for (int i = 0; i < 6; i++)
+	{
+		if (k == i)
+			continue;
+		float x = translate[i].x - translate[k].x;
+		float y = translate[i].y - translate[k].y;
+		float z = translate[i].z - translate[k].z;
+		float dist=glm::sqrt(glm::pow(x,2)+ glm::pow(y, 2)+ glm::pow(z, 2));
+		if (dist <= 5)
+		{
+			
+			collision[i] = true;
+			collisionResolutionSnowman(k);
+		}
+	}
+	if (translate[k].x > 0 && 45 - translate[k].x <= 5)
+	{
+		collision[k] = true;
+		collisionResolutionSnowman(k);
+	}
+
 	
+
+	if (35 - abs(translate[k].y) <= 5)
+	{
+		std::cout << abs(translate[k].y) << std::endl;
+
+		collision[k] = true;
+		collisionResolutionSnowman(k);
+	}
+	
+	
+}
+
+void animate(int i)
+{
+	/*
 	if (i == 0)
 	{
 		if (translate[i].y < 30 && rotation[i].y<90)
@@ -129,10 +194,46 @@ int animate(int i,int flag)
 		if (rotation[i].y >= 360)
 			rotation[i].y = 0.0f;
 
-	}
-	return flag;
+	}*/
+	translate[i].x = translate[i].x +0.01 * glm::sin(glm::radians(rotation[i].y));
+	translate[i].y = translate[i].y +0.01 * glm::cos(glm::radians(rotation[i].y));
+	collisionDetectionSnowman(i);
+	
 }
+float skyboxVertices[] =
+{
+	//   Coordinates
+	-1.0f, -1.0f,  1.0f,//        7--------6
+	 1.0f, -1.0f,  1.0f,//       /|       /|
+	 1.0f, -1.0f, -1.0f,//      4--------5 |
+	-1.0f, -1.0f, -1.0f,//      | |      | |
+	-1.0f,  1.0f,  1.0f,//      | 3------|-2
+	 1.0f,  1.0f,  1.0f,//      |/       |/
+	 1.0f,  1.0f, -1.0f,//      0--------1
+	-1.0f,  1.0f, -1.0f
+};
 
+unsigned int skyboxIndices[] =
+{
+	// Right
+	1, 2, 6,
+	6, 5, 1,
+	// Left
+	0, 4, 7,
+	7, 3, 0,
+	// Top
+	4, 5, 6,
+	6, 7, 4,
+	// Bottom
+	0, 3, 2,
+	2, 1, 0,
+	// Back
+	0, 1, 5,
+	5, 4, 0,
+	// Front
+	3, 7, 6,
+	6, 2, 3
+};
 int main()
 {
 	// Initialize GLFW
@@ -180,11 +281,12 @@ int main()
 	Shader lightShader("light.vert", "light.frag");
 	// Shader for hut
 	Shader hutS("default.vert", "default.frag");
-	// Shader for the first snowman
+	// Shader for the  snowmenn
 	
 	Shader snowManShader[] = { Shader("default.vert", "default.frag"),Shader("default.vert", "default.frag"),Shader("default.vert", "default.frag"),Shader("default.vert", "default.frag"),Shader("default.vert", "default.frag"),Shader("default.vert", "default.frag") };
 
-
+	// Sahder for the skybox
+	Shader skyboxS("SkyBox.vert","SkyBox.frag");
 
 
 
@@ -203,8 +305,88 @@ int main()
 	
 	
 	translate[0].x = 15.0f, translate[0].z = 0.0f, translate[0].y = 0.0f;
-	translate[1].x = 20.0f, translate[0].z = 0.0f, translate[0].y = 0.0f;
+	translate[1].x = 35.0f, translate[0].z = 0.0f, translate[0].y = 0.0f;
+	rotation[0].y = 90.0f;
+	rotation[1].y = 270.0f;
 	int flag = 0;
+
+	// Create VAO, VBO, and EBO for the skybox
+	unsigned int skyboxVAO, skyboxVBO, skyboxEBO;
+	glGenVertexArrays(1, &skyboxVAO);
+	glGenBuffers(1, &skyboxVBO);
+	glGenBuffers(1, &skyboxEBO);
+	glBindVertexArray(skyboxVAO);
+	glBindBuffer(GL_ARRAY_BUFFER, skyboxVBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(skyboxVertices), &skyboxVertices, GL_STATIC_DRAW);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, skyboxEBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(skyboxIndices), &skyboxIndices, GL_STATIC_DRAW);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
+
+	/*
+	
+	GL TEXTURE CUBEMAP POSITIVE X -- right 
+	GL TEXTURE CUBEMAP NEGATIVE X -- left 
+	GL TEXTURE CUBEMAP POSITIVE Y -- top  
+	GL TEXTURE CUBEMAP NEGATIVE Y -- bottom 
+	GL TEXTURE CUBEMAP POSITIVE Z -- front 
+	GL TEXTURE CUBEMAP NEGATIVE Z -- back
+	*/
+	std::string facesCubemap[6] =
+	{
+		"px.png", //Right
+		"nx.png", //Left
+		"py.png", //Top
+		"ny.png", //bottom
+		"pz.png",//front
+		"nz.png" //back
+	};
+
+	//Creation of the cubemap texture object
+	unsigned int cubemapTexture;
+	glGenTextures(1, &cubemapTexture);
+	glBindTexture(GL_TEXTURE_CUBE_MAP, cubemapTexture);
+	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+
+	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
+	// This might help with seams on some systems
+	//glEnable(GL_TEXTURE_CUBE_MAP_SEAMLESS);
+
+	// Cycles through all the textures and attaches them to the cubemap object
+	for (unsigned int i = 0; i < 6; i++)
+	{
+		int width, height, nrChannels;
+		unsigned char* data = stbi_load(facesCubemap[i].c_str(), &width, &height, &nrChannels, 0);
+		if (data)
+		{
+			stbi_set_flip_vertically_on_load(false);
+			glTexImage2D
+			(
+				GL_TEXTURE_CUBE_MAP_POSITIVE_X + i,
+				0,
+				GL_RGBA,
+				width,
+				height,
+				0,
+				GL_RGBA,
+				GL_UNSIGNED_BYTE,
+				data
+			);
+			stbi_image_free(data);
+		}
+		else
+		{
+			std::cout << "Failed to load texture: " << facesCubemap[i] << std::endl;
+			stbi_image_free(data);
+		}
+	}
 	while (!glfwWindowShouldClose(window))
 	{
 		// Specify the color of the background
@@ -260,13 +442,17 @@ int main()
 		glm::mat4 snowMan1M = glm::mat4(1.0f);
 		
 		
-		for (int i = 0; i < 3; i++)
+		for (int i = 0; i < 2; i++)
 		{
 			if (i <=1)
 			{
 				snowManShader[i].Activate();
 				glm::mat4 snowMan1M = glm::mat4(1.0f);
-				flag=animate(i,flag);
+				if (collision[i])
+					collisionResolutionSnowman(i);
+				else
+					animate(i);
+				
 				snowMan1M = glm::translate(snowMan1M, glm::vec3(translate[i].x, translate[i].z, translate[i].y));
 				snowMan1M = glm::rotate(snowMan1M, glm::radians(rotation[i].y), glm::vec3(0.0f, 1.0f, 0.0f));
 				snowMan1M = baseModel * snowMan1M;
@@ -301,6 +487,28 @@ int main()
 		//	
 		//}
 		// Swap the back buffer with the front buffer
+		glDepthFunc(GL_LEQUAL);
+
+		skyboxS.Activate();
+		glm::mat4 view = glm::mat4(1.0f);
+		glm::mat4 projection = glm::mat4(1.0f);
+		// We make the mat4 into a mat3 and then a mat4 again in order to get rid of the last row and column
+		// The last row and column affect the translation of the skybox (which we don't want to affect)
+		view = glm::mat4(glm::mat3(glm::lookAt(camera.Position, camera.Position + camera.Orientation, camera.Up)));
+		projection = glm::perspective(glm::radians(45.0f), (float)width / height, 0.1f, 100.0f);
+		glUniformMatrix4fv(glGetUniformLocation(skyboxS.ID, "view"), 1, GL_FALSE, glm::value_ptr(view));
+		glUniformMatrix4fv(glGetUniformLocation(skyboxS.ID, "projection"), 1, GL_FALSE, glm::value_ptr(projection));
+
+		// Draws the cubemap as the last object so we can save a bit of performance by discarding all fragments
+		// where an object is present (a depth of 1.0f will always fail against any object's depth value)
+		glBindVertexArray(skyboxVAO);
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_CUBE_MAP, cubemapTexture);
+		glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
+		glBindVertexArray(0);
+
+		// Switch back to the normal depth function
+		glDepthFunc(GL_LESS);
 		glfwSwapBuffers(window);
 		// Take care of all GLFW events
 		glfwPollEvents();
